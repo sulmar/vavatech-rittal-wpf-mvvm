49bda0875275d447ab3a7cb2b8caf7b4b68570ec
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-10 12:27:26 +0100

    FakeServices

---
+++ src/Rittal.Shop/Rittal.Shop.FakeServices/FakeCustomerService.cs
@@ -0,0 +1,44 @@
+﻿using Rittal.Shop.IServices;
+using Rittal.Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rittal.Shop.FakeServices
+{
+    public class FakeCustomerService : ICustomerService
+    {
+        private readonly ICollection<Customer> customers;
+
+        public FakeCustomerService()
+        {
+            customers = new List<Customer>();
+
+        }
+
+        public void Add(Customer entity)
+        {
+            customers.Add(entity);
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            return customers;
+        }
+
+        public Customer Get(int id)
+        {
+            return customers.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            customers.Remove(Get(id));
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
