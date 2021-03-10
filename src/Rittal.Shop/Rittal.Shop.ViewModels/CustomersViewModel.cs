26b9dcec45936432504d89ae48718f3672efcd09
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-10 14:25:15 +0100

    CustomersViewModel

--- src/Rittal.Shop/Rittal.Shop.ViewModels/CustomersViewModel.cs
+++ src/Rittal.Shop/Rittal.Shop.ViewModels/CustomersViewModel.cs
@@ -1,9 +1,34 @@
-﻿using System;
+﻿using Rittal.Shop.FakeServices;
+using Rittal.Shop.IServices;
+using Rittal.Shop.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Rittal.Shop.ViewModels
 {
 
     public class CustomersViewModel : BaseViewModel
     {
+        public IEnumerable<Customer> Customers { get; private set; }
+
+        private readonly ICustomerService customerService;
+
+        public CustomersViewModel()
+            : this(new FakeCustomerService())
+        {
+
+        }
+
+        public CustomersViewModel(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+
+            Load();
+        }
+
+        private void Load()
+        {
+            Customers = customerService.Get();
+        }
     }
 }
