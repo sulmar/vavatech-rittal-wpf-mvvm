ac48a2e8e42e37f7ce334cb14e86a570ccf75e36
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-11 13:52:40 +0100

    Refaktoring

--- src/Rittal.Shop/Rittal.Shop.WPFClient/Converters/RGBToBrushConverter.cs
+++ src/Rittal.Shop/Rittal.Shop.WPFClient/Converters/RGBToBrushConverter.cs
@@ -21,4 +21,18 @@ namespace Rittal.Shop.WPFClient.Converters
             throw new NotImplementedException();
         }
     }
+
+
+    public class RGBToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.FromRgb(System.Convert.ToByte(values[0]), System.Convert.ToByte(values[1]), System.Convert.ToByte(values[2]));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
