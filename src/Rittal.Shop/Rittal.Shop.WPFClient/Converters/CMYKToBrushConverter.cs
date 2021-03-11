981380b28d4b0487d26b4f1a0a547b4cfa38f533
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2021-03-11 13:44:57 +0100

    Fix

--- src/Rittal.Shop/Rittal.Shop.WPFClient/Converters/CMYKToBrushConverter.cs
+++ src/Rittal.Shop/Rittal.Shop.WPFClient/Converters/CMYKToBrushConverter.cs
@@ -9,11 +9,11 @@ using System.Windows.Media;
 
 namespace Rittal.Shop.WPFClient.Converters
 {
-    public class CMYKToBrushConverter : IMultiValueConverter
+    public class RGBToBrushConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return new SolidColorBrush(Color.FromArgb(0, (byte) values[0], (byte) values[1], (byte) values[2]));
+            return new SolidColorBrush(Color.FromRgb(System.Convert.ToByte(values[0]), System.Convert.ToByte(values[1]), System.Convert.ToByte(values[2])));
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
